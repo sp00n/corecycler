d4eb95e7412ca97def97bfb9497def383ce3736d
LucidLuxxx <joshuakline1991@gmail.com>
2025-04-03 17:22:12 -0400

    udpated to latest corecycler version
    added new setting in automatic test mode

--- helpers/automode-startup-script.ps1
+++ helpers/automode-startup-script.ps1
@@ -295,14 +295,9 @@ try {
 
     # Start the script now
     Write-Text('Command:')
-    Write-Text('Start-Process -PassThru -FilePath ''cmd.exe'' -ArgumentList @(''/C'', (''"' + $scriptRoot + '\Run CoreCycler.bat"''), ' + $coreTested + ')')
+    Write-Text('Start-Process -PassThru -FilePath ''cmd.exe'' -ArgumentList @(''/C'', (''"' + $scriptRoot + '\Run CoreCycler.bat" ' + $coreTested + ' && "' + $scriptRoot + '\CoreCycler.exe"''))')
 
-    $process = Start-Process -PassThru -FilePath 'cmd.exe' -ArgumentList @('/C', ('"' + $scriptRoot + '\Run CoreCycler.bat"'), $coreTested)
-
-    Write-Text('Additional Command:')
-    Write-Text('Start-Process -PassThru -FilePath ''"' + $scriptRoot + '\CoreCycler.exe"'' -ArgumentList @(' + $coreTested + ')')
-    
-    $processExe = Start-Process -PassThru -FilePath "$scriptRoot\CoreCycler.exe" -ArgumentList @($coreTested)
+    $process = Start-Process -PassThru -FilePath 'cmd.exe' -ArgumentList @('/C', ('"' + $scriptRoot + '\Run CoreCycler.bat" ' + $coreTested + ' && "' + $scriptRoot + '\CoreCycler.exe"'))
 }
 
 # Don't throw an error
